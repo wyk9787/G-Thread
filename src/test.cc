5edb4b5a68bbf44e1200ba88e2d290f760524650
Garrett Wang <wangyika@grinnell.edu>
2019-05-02 17:00:44 -0500

    Threads in a thread also works

--- src/test.cc
+++ src/test.cc
@@ -4,9 +4,13 @@
 #include "gstm.hh"
 #include "gthread.hh"
 
-#define THREAD_NUM 100
+#define THREAD_NUM 10
 
-void *fn(void *arg) {
+// Forward declaration
+void *fn1(void *);
+void *fn2(void *);
+
+void *fn2(void *arg) {
   int num = *(int *)arg;
   num++;
   *(int *)arg = num;
@@ -14,6 +18,26 @@ void *fn(void *arg) {
   *(int *)arg = num;
   num++;
   *(int *)arg = num;
+  return nullptr;
+}
+
+void *fn1(void *arg) {
+  int num = *(int *)arg;
+  num++;
+  *(int *)arg = num;
+  num++;
+  *(int *)arg = num;
+  num++;
+  *(int *)arg = num;
+
+  GThread threads[THREAD_NUM];
+
+  for (int i = 0; i < THREAD_NUM; i++) {
+    threads[i].Create(fn2, arg);
+  }
+  for (int i = 0; i < THREAD_NUM; i++) {
+    threads[i].Join();
+  }
 
   return nullptr;
 }
@@ -26,7 +50,7 @@ int main() {
   *a = 0;
 
   for (int i = 0; i < THREAD_NUM; i++) {
-    threads[i].Create(fn, a);
+    threads[i].Create(fn1, a);
   }
   for (int i = 0; i < THREAD_NUM; i++) {
     threads[i].Join();
